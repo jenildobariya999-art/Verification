1b498ce168ceb9673f53ab5f8a694e6f2b96abcf
jenildobariya999-art <jenildobariya999@gmail.com>
2026-03-16 22:03:45 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,36 +1,23 @@
 let bar=document.getElementById("bar")
-
 let percent=0
 
 let interval=setInterval(()=>{
 
 percent+=5
-
 bar.style.width=percent+"%"
 
 if(percent>=100){
 
 clearInterval(interval)
-
 verify()
 
 }
 
 },200)
 
-function fingerprint(){
-
-return navigator.userAgent+
-screen.width+
-screen.height+
-navigator.language
-
-}
-
 function verify(){
 
 let params=new URLSearchParams(window.location.search)
-
 let uid=params.get("uid")
 
 fetch("http://YOUR_SERVER_IP:8000/api_verify",{
@@ -42,19 +29,32 @@ headers:{
 },
 
 body:JSON.stringify({
-
-uid:uid,
-
-fingerprint:fingerprint()
-
+uid:uid
 })
 
 })
-
 .then(r=>r.text())
-.then(data=>{
+.then(result=>{
 
-document.getElementById("status").innerHTML=data
+if(result=="success"){
+
+document.body.innerHTML=`
+<h2>Verification Successful</h2>
+<a href="https://t.me/YOUR_BOT_USERNAME">
+<button>Back To Bot</button>
+</a>
+`
+
+}else{
+
+document.body.innerHTML=`
+<h2>Verification Failed</h2>
+<a href="https://t.me/YOUR_BOT_USERNAME">
+<button>Back To Bot</button>
+</a>
+`
+
+}
 
 })
 
