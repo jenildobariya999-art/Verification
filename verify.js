343f6178e5dcd08419ff7c858017246b295267a7
jenildobariya999-art <jenildobariya999@gmail.com>
2026-03-16 22:31:18 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,73 +1,32 @@
-let bar = document.getElementById("bar")
-let percent = 0
+let bar=document.getElementById("bar")
+let percent=0
 
-let interval = setInterval(()=>{
+let interval=setInterval(()=>{
 
-percent += 4
-bar.style.width = percent + "%"
+percent+=5
+bar.style.width=percent+"%"
 
-if(percent >= 100){
+if(percent>=100){
 clearInterval(interval)
 verify()
 }
 
 },120)
 
-function getFingerprint(){
+function fingerprint(){
 
-let canvas = document.createElement("canvas")
-let ctx = canvas.getContext("2d")
-
-ctx.textBaseline = "top"
-ctx.font = "14px Arial"
-ctx.fillText("device-check", 2, 2)
-
-let canvasFingerprint = canvas.toDataURL()
-
-return navigator.userAgent +
-navigator.language +
-navigator.platform +
-screen.width +
-screen.height +
-screen.colorDepth +
-canvasFingerprint
-
-}
-
-function detectEmulator(){
-
-let ua = navigator.userAgent.toLowerCase()
-
-if(
-ua.includes("android sdk") ||
-ua.includes("emulator") ||
-ua.includes("genymotion")
-){
-return true
-}
-
-return false
+return navigator.userAgent+
+navigator.language+
+screen.width+
+screen.height+
+navigator.platform
 
 }
 
 function verify(){
 
-let params = new URLSearchParams(window.location.search)
-let uid = params.get("uid")
-
-if(detectEmulator()){
-
-document.body.innerHTML = `
-<h2>Verification Failed</h2>
-<p>Emulator detected</p>
-<a href="https://t.me/Testing0011_ibot">
-<button>Back to Bot</button>
-</a>
-`
-
-return
-
-}
+let params=new URLSearchParams(window.location.search)
+let uid=params.get("uid")
 
 fetch("http://192.0.0.2:8000/verify",{
 
@@ -79,7 +38,7 @@ headers:{
 
 body:JSON.stringify({
 uid:uid,
-fingerprint:getFingerprint()
+fingerprint:fingerprint()
 })
 
 })
@@ -89,9 +48,8 @@ fingerprint:getFingerprint()
 
 if(data.status=="success"){
 
-document.body.innerHTML = `
+document.body.innerHTML=`
 <h2>Verification Successful</h2>
-<p>Your device verified successfully</p>
 <a href="https://t.me/Testing0011_ibot">
 <button>Back to Bot</button>
 </a>
@@ -99,9 +57,8 @@ document.body.innerHTML = `
 
 }else{
 
-document.body.innerHTML = `
+document.body.innerHTML=`
 <h2>Verification Failed</h2>
-<p>Duplicate device detected</p>
 <a href="https://t.me/Testing0011_ibot">
 <button>Back to Bot</button>
 </a>
