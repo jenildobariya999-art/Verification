3d46899518a6e03a3dbeb95835da99ee4e2741f9
jenildobariya999-art <jenildobariya999@gmail.com>
2026-03-30 14:44:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 let tg = window.Telegram.WebApp;
 let user = tg.initDataUnsafe.user;
 
-document.getElementById("name").innerText = user.first_name;
+document.getElementById("name").innerText = "👋 " + user.first_name;
+
+let steps = [
+  "Initializing...",
+  "Checking device...",
+  "Analyzing fingerprint...",
+  "Validating request...",
+  "Finalizing..."
+];
 
 let progress = 0;
 let bar = document.getElementById("bar");
 let percent = document.getElementById("percent");
 let status = document.getElementById("status");
+let stepText = document.getElementById("step");
+
+let i = 0;
 
 let interval = setInterval(() => {
-  progress += 5;
+  progress += 4;
   bar.style.width = progress + "%";
   percent.innerText = progress + "%";
+
+  if (i < steps.length) {
+    stepText.innerText = steps[i];
+    i++;
+  }
 
   if (progress >= 100) {
     clearInterval(interval);
     verify();
   }
-}, 150);
+}, 180);
 
 function getDevice() {
   return JSON.stringify({
@@ -32,7 +48,7 @@ function getDevice() {
 function verify() {
   percent.innerText = "Checking...";
 
-  fetch("https://web-production-155.up.railway.app//verify", {  // 🔥 CHANGE THIS
+  fetch("https://web-production-155.up.railway.app/verify", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -52,14 +68,15 @@ function verify() {
       status.className = "badge success";
       status.innerText = "VERIFIED";
 
-      document.getElementById("icon").innerText = "✅";
+      document.getElementById("icon").innerText = "🎉";
       document.getElementById("title").innerText = "Verification Successful";
-      document.getElementById("desc").innerText = "Device approved";
+      document.getElementById("desc").innerText = "Your device is approved";
+
     } else {
       status.className = "badge failed";
       status.innerText = "FAILED";
 
-      document.getElementById("icon").innerText = "❌";
+      document.getElementById("icon").innerText = "🚫";
       document.getElementById("title").innerText = "Verification Failed";
       document.getElementById("desc").innerText = "Device already used";
     }
@@ -68,4 +85,4 @@ function verify() {
 
 function closeApp() {
   tg.close();
-      }
+}
